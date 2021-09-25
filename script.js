ccdb06970c2c9e4b6ddf020c9089369ac6092b3d
enoripl <tomek00@gmail.com>
2021-09-25 18:25:55 +0200

    rewritten using loops

--- script.js
+++ script.js
@@ -16,26 +16,20 @@ function computerPlay() {
 // function for player prompt
 function playerPlay() {
   let playerChoice = prompt("Rock, paper or scissors?");
-  if (playerChoice.toUpperCase() === "rock".toUpperCase())
-    {
-        return "Rock";
-    }
-    else if (playerChoice.toUpperCase() === "paper".toUpperCase())
-    {
-        return "Paper";
-    }
-    else if (playerChoice.toUpperCase() === "scissors".toUpperCase())
-    {
-        return "Scissors";
-    }
+  if (playerChoice.toLowerCase() === "rock".toLowerCase()) {
+    return "Rock";
+  } else if (playerChoice.toLowerCase() === "paper".toLowerCase()) {
+    return "Paper";
+  } else if (playerChoice.toLowerCase() === "scissors".toLowerCase()) {
+    return "Scissors";
+  }
 }
 
 // one round of the game.
 function playRound(playerSelection, computerSelection) {
-  if
-    (playerSelection === computerSelection) {
+  if (playerSelection === computerSelection) {
     roundWinner = `tie`;
-    console.log(`${playerSelection} vs ${computerSelection} It's a tie!`)
+    console.log(`${playerSelection} vs ${computerSelection} It's a tie!`);
   } else if (
     (playerSelection === "Rock" && computerSelection === "Scissors") ||
     (playerSelection === "Paper" && computerSelection === "Rock") ||
@@ -43,25 +37,33 @@ function playRound(playerSelection, computerSelection) {
   ) {
     roundWinner = `player`;
     playerScore++;
-    console.log(`${playerSelection} beats ${computerSelection} You've won!`)
+    console.log(`${playerSelection} beats ${computerSelection} You've won!`);
   } else {
     roundWinner = `computer`;
     computerScore++;
-    console.log(`${playerSelection} loses to ${computerSelection} You've lost!`)
+    console.log(
+      `${playerSelection} loses to ${computerSelection} You've lost!`
+    );
   }
 }
 
 function displayWinner() {
   if (roundWinner === `player`) {
-   console.log(`You've won! Score is You: ${playerScore}, Computer: ${computerScore}`);
+    console.log(
+      `You've won! Score is You: ${playerScore}, Computer: ${computerScore}`
+    );
   } else if (roundWinner === `computer`) {
-    console.log(`You've lost! Score is You: ${playerScore}, Computer: ${computerScore}`);
-   } else if (roundWinner === `tie`) {
-     console.log(`It's a tie! Score is You: ${playerScore}, Computer: ${computerScore}`);
-   }
+    console.log(
+      `You've lost! Score is You: ${playerScore}, Computer: ${computerScore}`
+    );
+  } else if (roundWinner === `tie`) {
+    console.log(
+      `It's a tie! Score is You: ${playerScore}, Computer: ${computerScore}`
+    );
+  }
 }
 
-function game(){
+function game() {
   let playerSelection = playerPlay();
   let computerSelection = computerPlay();
   playRound(playerSelection, computerSelection);
@@ -70,16 +72,23 @@ function game(){
 
 function endGame() {
   if (playerScore > computerScore) {
-    console.log(`It's been 5 rounds, You've won!`)
+    console.log(`It's been 5 rounds, You've won!`);
   } else if (playerScore < computerScore) {
-    console.log(`It's been 5 rounds, Computer have won!`)
+    console.log(`It's been 5 rounds, Computer have won!`);
   } else {
-    console.log(`It's been 5 rounds and it's a tie!`)
+    console.log(`It's been 5 rounds and it's a tie!`);
   }
 }
-game();
-game();
-game();
-game();
-game();
-endGame(); 
+
+let i = 0;
+
+do {
+  if (i <= 4) {
+    game();
+  } else {
+    endGame();
+  }
+
+  i++;
+} while (i <= 4)
+
